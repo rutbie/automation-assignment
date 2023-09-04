fb61bfac8a491264c2094aea58784970acdc6d3c
unknown <rutabielyte@gmail.com>
2023-09-04 15:51:23 +0300

    Add POM for homepage & partners page

---
+++ src/components/Menu.ts
@@ -0,0 +1,21 @@
+import { Locator, Page, expect } from '@playwright/test';
+
+export default class Menu {
+	readonly component: Page;
+	readonly menuItems: Locator ;
+    readonly submenuItems: Locator;
+
+	constructor(component: Page) {
+		this.component = component;
+		this.menuItems = component.locator('.nav-link');
+		this.submenuItems = component.locator('[id^="menu-item-"]');
+	}
+
+    clickMenuItem = async (title: string) => {
+        await this.menuItems.locator(`text=${title}`).first().click();
+    }
+
+    clickSubmenuItem = async (title: string) => {
+        await this.submenuItems.locator(`text=${title}`).first().click();
+    }
+}
---
+++ src/pages/PlatformIntegrationsPage.ts
@@ -0,0 +1,54 @@
+import { Page, Locator, expect } from '@playwright/test';
+
+const pageRoute = '/partner-integrations';
+
+export const DROPDOWN_TITLES = {
+    PARTNER_TYPE: 'Partner Type',
+    INDUSTRY: 'Industry',
+    LOCATION: 'Location'
+};
+
+export default class PlatformIntegrationsPage {
+	readonly page: Page;
+    readonly searchInput: Locator;
+    readonly partnerListCards: Locator;
+
+	constructor(page: Page) {
+		this.page = page;
+        this.searchInput = page.locator('.search-input').first();
+        this.partnerListCards = page.locator('#partner-list .card');
+	}
+
+    getDropdown = async (title: string) => {
+        return await this.page.locator(`.selectpicker[title="${title}"]`).first();
+    }
+
+    fillSearchField = async (value: string) => {
+        await this.searchInput.fill(value);
+    }
+
+    selectOptions = async (dropdownTitle: string, options: string []) => {
+        const selectedOptions = [];
+        for (const option of options) {
+            selectedOptions.push(option);
+        }
+        await (await this.getDropdown(dropdownTitle)).selectOption(selectedOptions);
+    }
+
+    clickPartnerCardByNumber = async (number: number) => {
+        await this.partnerListCards.locator(`nth=${number-1}`).click();
+    }
+
+    clickPartnerCardByTitle = async (title: string) => {
+        await this.partnerListCards.locator(`text=${title}`).click();
+    }
+
+	assertDropdownToBeVisible = async (title: string) => {
+        await expect(await this.getDropdown(title)).toBeVisible();
+    }
+
+    assertAmountOfPartnersCard = async (amount: number) => {
+        await expect(await this.partnerListCards.count()).toBe(amount);
+    }
+    
+}
